07b57f49b7f3213e113c99d5be8e9d31a9127cab
Gianluca Castorina <gcastorina234@gmail.com>
2026-07-13 16:36:39 -0300

    Add Company Methods

---
+++ src/main/java/com/tech/point_system/security/user/_enum/Role.java
@@ -0,0 +1,7 @@
+package com.tech.point_system.security.user._enum;
+
+public enum Role {
+    USER,
+    COMPANY_ADMIN,
+    APP_ADMIN
+}
--- src/main/java/com/tech/point_system/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/tech/point_system/service/impl/CompanyServiceImpl.java
@@ -4,9 +4,12 @@ import com.tech.point_system.dto.company.CompanyDetailDTO;
 import com.tech.point_system.dto.company.CompanyListDTO;
 import com.tech.point_system.dto.company.CompanyRequestDTO;
 import com.tech.point_system.dto.company.CompanyUpdateDTO;
+import com.tech.point_system.exception.NotFoundException;
 import com.tech.point_system.mapper.CompanyMapper;
 import com.tech.point_system.model.Company;
 import com.tech.point_system.repository.CompanyRepository;
+import com.tech.point_system.security.user.model.User;
+import com.tech.point_system.security.user.repository.UserRepository;
 import com.tech.point_system.service.CompanyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,37 +17,53 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CompanyServiceImpl implements CompanyService {
     private final CompanyRepository repository;
     private final CompanyMapper mapper;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional
-    public CompanyDetailDTO addCompany(CompanyRequestDTO dto) {
+    public CompanyDetailDTO addCompany(String companyAdminId, CompanyRequestDTO dto) {
         Company company = mapper.toEntity(dto);
-
+        User proxyAdmin = userRepository.getReferenceById(companyAdminId);
+        company.setAdmin(proxyAdmin);
+        company = repository.save(company);
+        return mapper.toDetailDTO(company);
     }
 
     @Override
-    public CompanyDetailDTO updateCompany(Long id, CompanyUpdateDTO dto) {
-        return null;
+    @Transactional
+    public CompanyDetailDTO updateCompany(String companyAdminId, Long companyId, CompanyUpdateDTO dto) {
+        Company company = repository.findById(companyId).orElseThrow(() -> new NotFoundException("Company Not Found"));
+        mapper.updateEntityFromDTO(dto, company);
+        company = repository.save(company);
+        return mapper.toDetailDTO(company);
     }
 
     @Override
     public Page<CompanyListDTO> listCompanies(Pageable pageable) {
-        return null;
+        Page<Company> companies = repository.findAll(pageable);
+        if(companies.isEmpty()) {
+            return Page.empty();
+        }
+        return companies.map(mapper::toListDTO);
     }
 
     @Override
     public CompanyDetailDTO getCompany(Long id) {
-        return null;
+        Company company = repository.findById(id).orElseThrow(() -> new NotFoundException("Company Not Found"));
+        return mapper.toDetailDTO(company);
     }
 
     @Override
+    @Transactional
     public void deleteCompany(Long id) {
-
+        repository.deleteById(id);
     }
 }
